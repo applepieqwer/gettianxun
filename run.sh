1660a2aacf9383f7ed969d232ad6e5edf6d73adb
applepie <applepie@applepie-atom.lan>
2015-10-25 11:12:42 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -1,43 +1,43 @@
 python tianxun.py csha nyca 2015-12-23
-sleep 10
+sleep 30
 python tianxun.py csha nyca 2016-01-01
-sleep 10
+sleep 30
 python tianxun.py csha nyca 2016-02-06
-sleep 10
+sleep 30
 python tianxun.py csha nyca 2016-02-24
-sleep 10
+sleep 30
 
 python tianxun.py csha lond 2015-12-23
-sleep 10
+sleep 30
 python tianxun.py csha lond 2016-01-01
-sleep 10
+sleep 30
 python tianxun.py csha lond 2016-02-06
-sleep 10
+sleep 30
 python tianxun.py csha lond 2016-02-24
-sleep 10
+sleep 30
 
 python tianxun.py csha syda 2015-12-23
-sleep 10
+sleep 30
 python tianxun.py csha syda 2016-01-01
-sleep 10
+sleep 30
 python tianxun.py csha syda 2016-02-06
-sleep 10
+sleep 30
 python tianxun.py csha syda 2016-02-24
-sleep 10
+sleep 30
 
 python tianxun.py csha tyoa 2015-12-23
-sleep 10
+sleep 30
 python tianxun.py csha tyoa 2016-01-01
-sleep 10
+sleep 30
 python tianxun.py csha tyoa 2016-02-06
-sleep 10
+sleep 30
 python tianxun.py csha tyoa 2016-02-24
-sleep 10
+sleep 30
 
 python tianxun.py csha sins 2015-12-23
-sleep 10
+sleep 30
 python tianxun.py csha sins 2016-01-01
-sleep 10
+sleep 30
 python tianxun.py csha sins 2016-02-06
-sleep 10
+sleep 30
 python tianxun.py csha sins 2016-02-24
